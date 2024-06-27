c370485a5f3d46f5ec4eae83f4e205dfe24eeddb
rogers7levi <rogers.levi714@gmail.com>
2024-06-27 15:43:01 +0100

    Enter showing pop up message

--- practice.js
+++ practice.js
@@ -1,30 +1,8 @@
-var input15 = document.getElementById("1-5");
-var input25 = document.getElementById("2-5");
-var input35 = document.getElementById("3-5");
-var input45 = document.getElementById("4-5");
-var input55 = document.getElementById("5-5");
-var input14 = document.getElementById("1-4");
-var input24 = document.getElementById("2-4");
-var input34 = document.getElementById("3-4");
-var input44 = document.getElementById("4-4");
-var input54 = document.getElementById("5-4");
-var input13 = document.getElementById("1-3");
-var input23 = document.getElementById("2-3");
-var input33 = document.getElementById("3-3");
-var input43 = document.getElementById("4-3");
-var input53 = document.getElementById("5-3");
-var input12 = document.getElementById("1-2");
-var input22 = document.getElementById("2-2");
-var input32 = document.getElementById("3-2");
-var input42 = document.getElementById("4-2");
-var input52 = document.getElementById("5-2");
-var input11 = document.getElementById("1-1");
-var input21 = document.getElementById("2-1");
-var input31 = document.getElementById("3-1");
-var input41 = document.getElementById("4-1");
-var input51 = document.getElementById("5-1");
-
-
+var input1 = document.getElementById("1-5");
+var input2 = document.getElementById("2-5");
+var input3 = document.getElementById("3-5");
+var input4 = document.getElementById("4-5");
+var input5 = document.getElementById("5-5");
 
 function moveToNextBox(currentBox, nextBoxId) {
     var nextBox = document.getElementById(nextBoxId);
@@ -33,10 +11,10 @@ function moveToNextBox(currentBox, nextBoxId) {
     }
 }
 
-
 input1.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
+    alert("You have clicked!");
     document.getElementById("enterButton1" + input1.id.charAt(0)).click();
   }
 });
@@ -44,6 +22,7 @@ input1.addEventListener("keypress", function (event) {
 input2.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
+    alert("You have clicked!");
     document.getElementById("enterButton2" + input2.id.charAt(0)).click();
   }
 });
@@ -51,6 +30,7 @@ input2.addEventListener("keypress", function (event) {
 input3.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
+    alert("You have clicked!");
     document.getElementById("enterButton3" + input3.id.charAt(0)).click();
   }
 });
@@ -58,6 +38,7 @@ input3.addEventListener("keypress", function (event) {
 input4.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
+    alert("You have clicked!");
     document.getElementById("enterButton4" + input4.id.charAt(0)).click();
   }
 });
@@ -65,6 +46,18 @@ input4.addEventListener("keypress", function (event) {
 input5.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
+    alert("You have clicked!");
     document.getElementById("enterButton5" + input5.id.charAt(0)).click();
   }
 });
+
+function guessThisWord(word) {
+const keys = word.split('');
+for(let i=0; i < keys.length; i++){
+    const event = new KeyboardEvent('keydown', {key:keys[i]});
+    document.dispatchEvent(event);
+}
+const enterEvent = new KeyboardEvent('keydown', {key: 'Enter'});
+}
+ guessThisWord('hello')
+
